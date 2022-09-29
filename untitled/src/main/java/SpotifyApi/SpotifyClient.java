47b622944f17c1580147760a67ecbdb9968c3119
Aji_Kat <oussamabendaou63@gmail.com>
2022-09-29 22:01:39 +0100

    new

--- untitled/src/main/java/SpotifyApi/SpotifyClient.java
+++ untitled/src/main/java/SpotifyApi/SpotifyClient.java
@@ -2,19 +2,90 @@ package SpotifyApi;
 
 import org.apache.hc.core5.http.ParseException;
 import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.SpotifyHttpManager;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import se.michaelthelin.spotify.model_objects.miscellaneous.CurrentlyPlaying;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRefreshRequest;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import se.michaelthelin.spotify.requests.data.player.GetUsersCurrentlyPlayingTrackRequest;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class SpotifyClient implements SpotifyMusicPlayerInt
 {
     private static SpotifyApi spotifyApi;
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://example.com/callback/");
+
+
+    public boolean newUser()
+    {
+        final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().build();
+        final URI uri = authorizationCodeUriRequest.execute();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("URI: " + uri.toString());
+        System.out.println("Please Enter code :");
+        try {
+            String code = new String(reader.readLine());
+
+            final AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            TokenManager.setNewRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            return true;
+
+        }
+        catch (IOException | SpotifyWebApiException | ParseException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
+
+        return false;
+    }
+
+    public boolean refreshToken()
+    {
+        try {
+
+
+
+            spotifyApi.setRefreshToken(TokenManager.getRefreshToken());
+            System.out.println("Refresh token : " + spotifyApi.getRefreshToken());
+            AuthorizationCodeRefreshRequest authorizationCodeRefreshRequest = spotifyApi.authorizationCodeRefresh().build();
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRefreshRequest.execute();
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            TokenManager.setNewRefreshToken(authorizationCodeCredentials.getRefreshToken());
+
+        }
+        catch (IOException | SpotifyWebApiException | ParseException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return false;
+    }
+
+
+
+
     public SpotifyClient(String clientId,String clientSecret){
          spotifyApi = new SpotifyApi.Builder()
                 .setClientId(clientId)
                 .setClientSecret(clientSecret)
+                 .setRedirectUri(redirectUri)
                 .build();
     }
     public SpotifyApi getSpotifyApi(){
--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import SpotifyApi.SearchSpotifyTrack;
 import  SpotifyApi.SpotifyClient;
 import SpotifyApi.SpotifyMusicPlayer;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.Objects;
 
@@ -15,8 +17,44 @@ public class Main {
             args [2] Track  Name
         */
         SpotifyClient spotifyClient = new SpotifyClient(args[0],args[1]);
+        File file = new File("token.txt");
+
+        if(! file.exists())
+        {
+            try {
+                boolean b = file.createNewFile();
+                if(b)
+                {
+                    spotifyClient.newUser();
+                }
+            }
+            catch (IOException | SecurityException ex)
+            {
+                System.out.printf("Error :%s%n",ex.getMessage());
+            }
+
+        }
+        else
+        {
+            if(file.length() == 0)
+            {
+                spotifyClient.newUser();
+            }
+            else
+            {
+                spotifyClient.refreshToken();
+            }
+
+            System.out.println("Already exist");
+
+        }
+
+
+
+
+        /*
         //SpotifyMusicPlayer.queueAdd(spotifyClient.getSpotifyApi(), Objects.requireNonNull(SearchSpotifyTrack.SearchTrack(spotifyClient.getSpotifyApi(), args[2])));
-        spotifyClient.getCurrentMusicInfo();
+        //spotifyClient.getCurrentMusicInfo();*/
 
     }
 
